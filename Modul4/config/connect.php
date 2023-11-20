3454a962b270fdf70368283d0ef0e809356a937d
Diamonicly <thebutteripad@gmail.com>
2023-11-20 09:21:57 +0700

    Push MODUL4

---
+++ Modul4/config/connect.php
@@ -0,0 +1,19 @@
+<!-- File ini berisi koneksi dengan database MySQL -->
+<?php 
+
+// (1) Buatlah variable untuk connect ke database yang telah di import ke phpMyAdmin
+$host = "localhost:3306";
+$username = "root";
+$password = "";
+$dbname = "modul4_jurnal";
+$db = new mysqli($host, $username, $password, $dbname);
+
+// 
+
+// (2) Buatlah perkondisian untuk menampilkan pesan error ketika database gagal terkoneksi
+if ($db ->connect_error){
+    die("Koneksi Gagal: " . mysqli_connect_error());
+}
+// 
+ 
+?>
